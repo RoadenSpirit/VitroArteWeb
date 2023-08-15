538f16a4a8140ec0622a611985dfa97cf43f02bd
RoadenSpirit <135933087+RoadenSpirit@users.noreply.github.com>
2023-08-14 21:07:39 -0600

    Multiples Cambios Time Line 80%

--- assets/js/carouselVitral.js
+++ assets/js/carouselVitral.js
@@ -1,33 +1,45 @@
 const fila = document.querySelector('.contenedor-carousel');
-const peliculas = document.querySelectorAll('.pelicula');
-
+const imagen = document.querySelectorAll('.imagen');
 const flechaIzquierda = document.getElementById('flecha-izquierda');
 const flechaDerecha = document.getElementById('flecha-derecha');
-
+let velocidadDesplazamiento = 10;
 // ? ----- ----- Event Listener para la flecha derecha. ----- -----
-flechaDerecha.addEventListener('click', () => {
-	fila.scrollLeft += fila.offsetWidth;
+let intervaloDesplazamiento;
 
-	const indicadorActivo = document.querySelector('.indicadores .activo');
-	if(indicadorActivo.nextSibling){
-		indicadorActivo.nextSibling.classList.add('activo');
-		indicadorActivo.classList.remove('activo');
-	}
+flechaDerecha.addEventListener('mousedown', () => {
+    intervaloDesplazamiento = setInterval(() => {
+		fila.scrollLeft += fila.offsetWidth / 2 / velocidadDesplazamiento; 
+        const indicadorActivo = document.querySelector('.indicadores .activo');
+        if (indicadorActivo.nextSibling) {
+            indicadorActivo.nextSibling.classList.add('activo');
+            indicadorActivo.classList.remove('activo');
+        }
+    }, 100); // Ajusta el intervalo de tiempo según tus necesidades (en milisegundos)
 });
+
+flechaDerecha.addEventListener('mouseup', () => {
+    clearInterval(intervaloDesplazamiento);
+});
+
 
 // ? ----- ----- Event Listener para la flecha izquierda. ----- -----
-flechaIzquierda.addEventListener('click', () => {
-	fila.scrollLeft -= fila.offsetWidth;
 
-	const indicadorActivo = document.querySelector('.indicadores .activo');
-	if(indicadorActivo.previousSibling){
-		indicadorActivo.previousSibling.classList.add('activo');
-		indicadorActivo.classList.remove('activo');
-	}
+flechaIzquierda.addEventListener('mousedown', () => {
+    intervaloDesplazamiento = setInterval(() => {
+        fila.scrollLeft -= fila.offsetWidth / 2 / velocidadDesplazamiento; // Ajusta la velocidad de desplazamiento según tus necesidades
+        const indicadorActivo = document.querySelector('.indicadores .activo');
+        if (indicadorActivo.previousSibling) {
+            indicadorActivo.previousSibling.classList.add('activo');
+            indicadorActivo.classList.remove('activo');
+        }
+    }, 100); // Ajusta el intervalo de tiempo según tus necesidades (en milisegundos)
 });
 
+flechaIzquierda.addEventListener('mouseup', () => {
+    clearInterval(intervaloDesplazamiento);
+});
 // ? ----- ----- Paginacion ----- -----
-const numeroPaginas = Math.ceil(peliculas.length / 5);
+const numeroPaginas = Math.ceil(imagen.length / 2);
 for(let i = 0; i < numeroPaginas; i++){
 	const indicador = document.createElement('button');
 
@@ -44,17 +56,13 @@ for(let i = 0; i < numeroPaginas; i++){
 	});
 }
 
-// ? ----- ----- Hover ----- -----
-peliculas.forEach((pelicula) => {
-	pelicula.addEventListener('mouseenter', (e) => {
-		const elemento = e.currentTarget;
-		setTimeout(() => {
-			peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
-			elemento.classList.add('hover');
-		}, 300);
-	});
-});
+// Movimiento automatico 
+window.addEventListener('scroll', function() {
+    const carousel = document.getElementById('carousel');
+    const carruselPosicion = carousel.getBoundingClientRect().top;
+    const ventanaAltura = window.innerHeight;
 
-fila.addEventListener('mouseleave', () => {
-	peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+    if (carruselPosicion < ventanaAltura) {
+        console.log('El carrusel es visible en el sitio');
+    }
 });
